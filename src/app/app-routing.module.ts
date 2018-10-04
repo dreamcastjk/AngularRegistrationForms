b77b5c9264ecf04f9c9ff595ccf4e1505ffcc37f
dreamcast <slipdoll@gmail.com>
2018-10-04 21:13:22 +0300

    two pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthorizeComponent} from './components/authorize/authorize.component';
+import {RegisterComponent} from './components/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'authorize',
+    component: AuthorizeComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    component: AuthorizeComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {MatButtonModule} from '@angular/material/button';
+
+
+import { AuthorizeComponent } from './components/authorize/authorize.component';
+import { RegisterComponent } from './components/register/register.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    NgbModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
